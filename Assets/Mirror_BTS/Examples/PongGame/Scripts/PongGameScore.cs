7dba5f067e3e7d95d0b16dcd1dbbf2b2a88bb8a6
Robert Wiebe <admin1@burningthumb.com>
2022-12-21 11:50:33 -0800

    Pong audio

---
+++ Assets/Mirror_BTS/Examples/PongGame/Scripts/PongGameScore.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+
+namespace Mirror.Examples.PongGame
+{
+    public class PongGameScore : NetworkBehaviour
+    {
+        [SerializeField] int m_value;
+        [SerializeField] PongGameScoreDigit m_tens;
+        [SerializeField] PongGameScoreDigit m_ones;
+
+        [SerializeField] AudioSource m_scoreAudio;
+
+        public int Value
+        {
+            get
+            {
+                return m_value;
+            }
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            if (null == m_scoreAudio)
+            {
+                m_scoreAudio = GetComponent<AudioSource>();
+            }
+
+            SetDigitsToValue();
+        }
+
+        private void SetDigitsToValue()
+        {
+            m_tens.SetValue(m_value / 10);
+            m_ones.SetValue(m_value - (m_value / 10) * 10);
+        }
+
+        private void PlayAudio()
+        {
+            if (m_value > 0)
+            {
+                if (null != m_scoreAudio)
+                {
+                    m_scoreAudio.Play();
+                }
+            }
+        }
+
+
+        public void SetValue(int a_int)
+        {
+            m_value = a_int;
+
+            PlayAudio();
+            SetDigitsToValue();
+        }
+
+        [ClientRpc]
+        public void rpcSetValue(int a_int)
+        {
+            SetValue(a_int);
+        }
+    }
+}
