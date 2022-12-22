83f90c396c57716bfd89e8945eb952ce60479587
Robert Wiebe <admin1@burningthumb.com>
2022-12-21 16:24:32 -0800

    Menu work

---
+++ Assets/Mirror_BTS/Examples/PongGame/Scripts/Managers/InterfaceManager.cs
@@ -0,0 +1,38 @@
+
+using UnityEngine;
+
+public class InterfaceManager : MonoBehaviour
+{
+	public UIScreen mainMenu;
+	public UIScreen pauseMenu;
+	public UIScreen lobbyMenu;
+
+	public static InterfaceManager Instance => Singleton<InterfaceManager>.Instance;
+
+	private void Start()
+	{
+	}
+
+	public void OpenPauseMenu()
+	{
+		// open pause menu only if the kart can drive and the menu isn't open already
+		if (UIScreen.activeScreen != pauseMenu)
+		{
+			UIScreen.Focus(pauseMenu);
+		}
+	}
+
+	public void Quit()
+	{
+#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+#endif
+		Application.Quit();
+	}
+
+	// Audio Hooks
+	public void SetVolumeMaster(float value) => AudioManager.SetVolumeMaster(value);
+	public void SetVolumeSFX(float value) => AudioManager.SetVolumeSFX(value);
+	public void SetVolumeUI(float value) => AudioManager.SetVolumeUI(value);
+	public void SetVolumeMusic(float value) => AudioManager.SetVolumeMusic(value);
+}
