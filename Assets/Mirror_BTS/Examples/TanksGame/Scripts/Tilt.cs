a5ff144fc386d9949f62f0780743d1073c2db33b
Robert Wiebe <admin1@burningthumb.com>
2023-01-13 14:13:09 -0800

    Fix a fightng, lighting, and over tilting

--- Assets/Mirror_BTS/Examples/TanksGame/Scripts/Tilt.cs
+++ Assets/Mirror_BTS/Examples/TanksGame/Scripts/Tilt.cs
@@ -1,22 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
- 
+
 public class Tilt : MonoBehaviour
 {
     public LayerMask mask;
     public float speed = 1.0f;
- 
+
     void LateUpdate()
     {
         RaycastHit hit;
 
         if (Physics.Raycast(transform.position, -transform.up, out hit))
         {
-            var slopeRotation = Quaternion.FromToRotation(transform.up, hit.normal);
-    
-           transform.rotation = Quaternion.Slerp(transform.rotation, slopeRotation * transform.rotation, speed * Time.deltaTime);
-            
+
+            if ((hit.normal.x < 0) || (hit.normal.y < 0) || (hit.normal.z < 0))
+            {
+            }
+            else
+            {
+                var slopeRotation = Quaternion.FromToRotation(transform.up, hit.normal);
+                transform.rotation = Quaternion.Slerp(transform.rotation, slopeRotation * transform.rotation, speed * Time.deltaTime);
+            }
+
         }
     }
 }
