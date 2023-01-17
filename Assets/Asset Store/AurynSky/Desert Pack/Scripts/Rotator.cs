627434aa23004bd224726b44d90574a9a4fb9685
Robert Wiebe <admin1@burningthumb.com>
2023-01-16 21:17:08 -0800

    Prior to WaterQuadPixies

--- Assets/Asset Store/AurynSky/Desert Pack/Scripts/Rotator.cs
+++ Assets/Asset Store/AurynSky/Desert Pack/Scripts/Rotator.cs
@@ -2,15 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rotator : MonoBehaviour {
+namespace com.AurynSky.DesertPack
+{
+    public class Rotator : MonoBehaviour
+    {
 
-	public Vector3 direction;
-	public float speed;
+        public Vector3 direction;
+        public float speed;
 
-	
-	// Update is called once per frame
-	void Update () 
-	{
-		transform.Rotate(direction * speed * Time.deltaTime);
-	}
+
+        // Update is called once per frame
+        void Update()
+        {
+            transform.Rotate(direction * speed * Time.deltaTime);
+        }
+    }
 }
