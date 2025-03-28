5277e3512e5520c3d7f921dd105c9f024713b7e0
Robert Wiebe <admin1@burningthumb.com>
2025-03-28 09:25:53 -0700

    Fix bundle id and tv remote setup for tvOS

---
+++ Assets/Mirror_BTS/Examples/TanksGame/Scripts/BTS_TVRemoteSetup.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BTS_TVRemoteSetup : MonoBehaviour
+{
+	public bool m_allowExitToHome = false;
+	public bool m_allowRemoteRotation = true;
+    public bool m_reportAbsoluteDpadValues = false;
+    public bool m_touchesEnabled = true;
+
+		// Start is called before the first frame update
+		void Start()
+    {
+#if UNITY_TVOS
+				UnityEngine.tvOS.Remote.allowExitToHome = m_allowExitToHome;
+				UnityEngine.tvOS.Remote.allowRemoteRotation = m_allowRemoteRotation;
+				UnityEngine.tvOS.Remote.reportAbsoluteDpadValues = m_reportAbsoluteDpadValues;
+				UnityEngine.tvOS.Remote.touchesEnabled = m_touchesEnabled;
+#endif
+		}
+
+
+}
