a32c543b008ab70aaa5d1fcbacb2cd8d14ebf8a1
Robert Wiebe <admin1@burningthumb.com>
2025-03-31 10:34:04 -0700

    Update BTSAds and TopShelf

--- Assets/Ads/SciptableObjects/Scripts/SOBTSAd.cs
+++ Assets/Ads/SciptableObjects/Scripts/SOBTSAd.cs
@@ -6,30 +6,60 @@ using UnityEngine.UI;
 [CreateAssetMenu(fileName = "New SOBTSAd", menuName = "SO/BTS Ad")]
 public class SOBTSAd : ScriptableObject
 {
-    public enum GAMEID { none, ad2442, canyon_run, coconut_hut, idrone, karts, mars_2055, net_pong, net_tanks}
-    public enum MEDIUM { affiliate}
-    public enum ADID   { none, ad2442, canyon_run, coconut_hut, drive_download, idrone, karts, mars_2055, mireth_music, net_pong, netshred, net_tanks, shredit, videokiosk}
-    public enum ADVARIANT { _01 }
+    [System.Serializable]
+    public class PlatformAlternate
+    {
+        public RuntimePlatform[] m_platforms;
+        public SOBTSAd m_btsAd;
+    }
 
-    public enum ADDITIONALPLATFORMS { AndroidTV, FireOS}
+    public enum GAMEID { none, ad2442, canyon_run, coconut_hut, idrone, karts, mars_2055, net_pong, net_tanks }
+    public enum MEDIUM { affiliate }
+    public enum ADID { none, ad2442, canyon_run, coconut_hut, drive_download, idrone, karts, mars_2055, mireth_music, net_pong, netshred, net_tanks, shredit, videokiosk }
+    public enum ADVARIANT { _01 }
+    public enum ADDITIONALPLATFORMS { AndroidTV, FireOS }
 
     [SerializeField] string m_title = "Ad Title";
     [SerializeField] string m_subtitle = "Ad Subtitle";
-    [TextArea(1, 4)] [SerializeField] string m_blurb = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-
+    [TextArea(1, 4)] [SerializeField] string m_blurb = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
     [SerializeField] Sprite m_adSprite;
     [SerializeField] string m_url = "https://www.domain.com/";
-
     [SerializeField] GAMEID m_gameID = GAMEID.none;
     [SerializeField] MEDIUM m_medium = MEDIUM.affiliate;
     [SerializeField] ADID m_adID = ADID.none;
     [SerializeField] ADVARIANT m_adVariant = ADVARIANT._01;
+    [SerializeField] PlatformAlternate[] m_platformAlternatives;
+
+    // Helper method to get the applicable ad based on platform
+    private SOBTSAd GetPlatformSpecificAd()
+    {
+        if (m_platformAlternatives == null || m_platformAlternatives.Length == 0)
+            return this;
+
+        RuntimePlatform currentPlatform = Application.platform;
+
+        foreach (var alternate in m_platformAlternatives)
+        {
+            if (alternate.m_platforms != null)
+            {
+                foreach (var platform in alternate.m_platforms)
+                {
+                    if (platform == currentPlatform)
+                    {
+                        return alternate.m_btsAd != null ? alternate.m_btsAd : this;
+                    }
+                }
+            }
+        }
+
+        return this; // Default to this ad if no match
+    }
 
     public string Title
     {
         get
         {
-            return m_title;
+            return GetPlatformSpecificAd().m_title;
         }
     }
 
@@ -37,7 +67,7 @@ public class SOBTSAd : ScriptableObject
     {
         get
         {
-            return m_subtitle;
+            return GetPlatformSpecificAd().m_subtitle;
         }
     }
 
@@ -45,7 +75,7 @@ public class SOBTSAd : ScriptableObject
     {
         get
         {
-            return m_blurb;
+            return GetPlatformSpecificAd().m_blurb;
         }
     }
 
@@ -53,7 +83,7 @@ public class SOBTSAd : ScriptableObject
     {
         get
         {
-            return m_adSprite;
+            return GetPlatformSpecificAd().m_adSprite;
         }
     }
 
@@ -61,56 +91,54 @@ public class SOBTSAd : ScriptableObject
     {
         get
         {
-            return m_url;
+            return GetPlatformSpecificAd().m_url;
         }
     }
 
     public string GameID
-	{
+    {
         get
-		{
-            return m_gameID.ToString();
-		}
-	}
+        {
+            return GetPlatformSpecificAd().m_gameID.ToString();
+        }
+    }
 
     public string Medium
-	{
-		get
-		{
-            return m_medium.ToString();
-		}
-	}
+    {
+        get
+        {
+            return GetPlatformSpecificAd().m_medium.ToString();
+        }
+    }
 
     public string AdID
-	{
+    {
         get
-		{
-            return $"{m_adID.ToString()}{m_adVariant.ToString()}";
+        {
+            SOBTSAd l_ad = GetPlatformSpecificAd();
+            return $"{l_ad.m_adID.ToString()}{l_ad.m_adVariant.ToString()}";
         }
-
     }
 
     public string Platform
-	{
+    {
         get
-		{
+        {
             string l_result = Application.platform.ToString();
 
             if (RuntimePlatform.Android == Application.platform)
-			{
+            {
                 if (BTS_TV_Type.IsAmazonFireTV)
-				{
+                {
                     l_result = ADDITIONALPLATFORMS.FireOS.ToString();
-				}
+                }
                 else if (BTS_TV_Type.IsAndroidTV)
-				{
+                {
                     l_result = ADDITIONALPLATFORMS.AndroidTV.ToString();
-				}
-			}
+                }
+            }
 
             return l_result;
-		}
-	}
-
-    
+        }
+    }
 }
