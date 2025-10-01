38c5eff520cf46bc20d0e128d5323556cc71e4cb
Robert Wiebe <admin1@burningthumb.com>
2025-10-01 16:21:16 -0700

    Handle running Ads for some scenes

--- Assets/Mirror_BTS/Examples/TanksGame/Scripts/BTSSelectScene.cs
+++ Assets/Mirror_BTS/Examples/TanksGame/Scripts/BTSSelectScene.cs
@@ -14,8 +14,11 @@ public class SceneInfo
 {
     public string m_UIName;
     [TextArea] public string m_UIDescription;
+    public string m_UIButtonHint;
     public Sprite m_UISprite;
     public string m_SceneName;
+    public bool m_AdSupported;
+
 }
 
 public class BTSSelectScene : MonoBehaviour
@@ -25,6 +28,7 @@ public class BTSSelectScene : MonoBehaviour
 
     [Header("UI References")]
     [SerializeField] private TMP_Text m_UINameText;
+    [SerializeField] private TMP_Text m_UIButtonHintText;
     [SerializeField] private TMP_Text m_UIDescriptionText;
     [SerializeField] private Image m_UISpriteImage;
 
@@ -32,8 +36,25 @@ public class BTSSelectScene : MonoBehaviour
 
     private static BTSSelectScene s_Instance;
 
+    public bool AdSupported
+    {
+        get
+        {
+            if (null == m_Scenes || 0 == m_Scenes.Length) return false;
+            SceneInfo l_Info = m_Scenes[m_CurrentIndex];
+
+            return l_Info.m_AdSupported;
+        }
+    }
+
     private void Awake()
     {
+        if (null != s_Instance)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
         s_Instance = this;
     }
 
@@ -68,7 +89,41 @@ public class BTSSelectScene : MonoBehaviour
         }
     }
 
+    public void PlayAdOrLoadScene()
+    {
+        if (null == m_Scenes || 0 == m_Scenes.Length) return;
+
+        SceneInfo l_Info = m_Scenes[m_CurrentIndex];
+
+        if (!string.IsNullOrEmpty(l_Info.m_SceneName))
+        {
+            if (AdSupported)
+            {
+                AdmobAdSingletonUI l_ui = GetComponent<AdmobAdSingletonUI>();
+
+                if (null != l_ui)
+                { 
+                    l_ui.ShowRV();
+                }
+            }
+            else
+            { 
+                SceneManager.LoadScene(l_Info.m_SceneName);
+            }
+        }
+    }
+
     // --- Class Methods (forward to instance) ---
+
+    public static bool IsAdSupported()
+    {
+        if (null != s_Instance)
+        {
+            return s_Instance.AdSupported;
+        }
+
+        return false;
+    }
 
     public static void Next()
     {
@@ -94,6 +149,14 @@ public class BTSSelectScene : MonoBehaviour
         }
     }
 
+    public static void AdOrLoad()
+    {
+        if (null != s_Instance)
+        {
+            s_Instance.PlayAdOrLoadScene();
+        }
+    }
+
     // --- Helpers ---
 
     private void UpdateUI()
@@ -102,6 +165,7 @@ public class BTSSelectScene : MonoBehaviour
         SceneInfo l_Info = m_Scenes[m_CurrentIndex];
 
         if (null != m_UINameText) m_UINameText.text = l_Info.m_UIName;
+        if (null != m_UIButtonHintText) m_UIButtonHintText.text = l_Info.m_UIButtonHint;
         if (null != m_UIDescriptionText) m_UIDescriptionText.text = l_Info.m_UIDescription;
         if (null != m_UISpriteImage) m_UISpriteImage.sprite = l_Info.m_UISprite;
     }
